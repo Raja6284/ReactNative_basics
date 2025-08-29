0f4e1603ddde33bcbc0734ce68a1fc6b4972e70b
Raja Kumar <rajaraja360parth@gmail.com>
2025-08-29 12:40:54 +0530

    sample contact list ui

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import FlatCards from './components/FlatCards'
 import ElevatedCards from './components/ElevatedCards'
 import FancyCards from './components/FancyCards'
 import ActionCards from './components/ActionCards'
+import ContactList from './components/ContactLists'
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
          <FlatCards/>
          <ElevatedCards/>
          <FancyCards/>
+         <ContactList />
          <ActionCards />
       </ScrollView>
      
